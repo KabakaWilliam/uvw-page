c1fa76445bf0c9bb0467602de4908259e44c9ea2
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-06-07 20:31:39 +0300

    Added new globe

--- components/FaceBubble.tsx
+++ components/FaceBubble.tsx
@@ -1,6 +1,30 @@
-const FaceBubble = () => {
+import { useEffect, useState } from "react";
+
+interface props {
+  imgLink: string;
+}
+const FaceBubble = ({ imgLink }: props) => {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHidden(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className=" w-[60px] h-[60px]  rounded-full bg-gray-300 z-40"></div>
+    <div
+      className={
+        !hidden ? " w-[60px] h-[60px]  rounded-full bg-gray-300 z-40" : "hidden"
+      }
+    >
+      <img
+        src={imgLink}
+        alt=""
+        className="w-[100%] h-[100%] object-cover rounded-full"
+      />
+    </div>
   );
 };
 
