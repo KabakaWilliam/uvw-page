403d458a6f74eba3ef5525dd2d52fb96ed440c51
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-08-06 22:46:03 +0300

    debugged page

--- components/CarouselComponent.tsx
+++ components/CarouselComponent.tsx
@@ -6,50 +6,6 @@ import userData from "../Data/sampleData.json";
 
 const CarouselComponent = () => {
   const [selectedUser, setSelectedUser] = useRecoilState(SelectedUserState);
-  const maxScrollWidth = useRef(0);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const carousel = useRef<HTMLDivElement>(null);
-
-  const movePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prevState) => prevState - 1);
-    }
-  };
-
-  const moveNext = () => {
-    if (
-      carousel.current !== null &&
-      carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
-    ) {
-      setCurrentIndex((prevState) => prevState + 1);
-    }
-  };
-
-  const isDisabled = (direction: string) => {
-    if (direction === "prev") {
-      return currentIndex <= 0;
-    }
-
-    if (direction === "next" && carousel.current !== null) {
-      return (
-        carousel.current.offsetWidth * currentIndex >= maxScrollWidth.current
-      );
-    }
-
-    return false;
-  };
-
-  useEffect(() => {
-    if (carousel !== null && carousel.current !== null) {
-      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
-    }
-  }, [currentIndex]);
-
-  useEffect(() => {
-    maxScrollWidth.current = carousel.current
-      ? carousel.current.scrollWidth - carousel.current.offsetWidth
-      : 0;
-  }, []);
 
   return (
     <div className="carousel my-[7vh] mx-auto">
@@ -58,7 +14,11 @@ const CarouselComponent = () => {
           {selectedUser.userMedia ? (
             <Carousel slide={false}>
               {selectedUser.userMedia.map((link) => (
-                <img src={link} className=" w-[627px] h-[448px] object-cover" />
+                <img
+                  key={Math.random()}
+                  src={link}
+                  className=" w-[627px] h-[448px] object-cover"
+                />
               ))}
             </Carousel>
           ) : null}
